7c2cdad1ccebbdb19cc03710b457af42373dc665
Seerat Jaan <meersuhana266@gmail.com>
2025-01-24 15:30:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function calculator(num1, num2, operator) {
+  if (operator == "+") {
+    console.log(num1 + num2);
+  } else if (operator == "-") {
+    console.log(num1 - num2);
+  } else if (operator == "*") {
+    console.log(num1 * num2);
+  } else if (operator == "/") {
+    console.log(num1 / num2);
+  } else {
+    console.log("invalid operator");
+  }
+}
+calculator(5, 5, "+");
